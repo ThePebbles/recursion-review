d2b864ff5f4fbf62fd4b3b8a0f9a0cdd7d1fec5b
Anthony Bui <anthonyb50@gmail.com>
2022-10-28 15:11:15 -0400

    Complete StringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,51 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    result += obj;
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    result += '[';
+    for (var i = 0; i < obj.length; i++) {
+      if (typeof obj[i] === 'object' || typeof obj[i] === 'string') {
+        result += stringifyJSON(obj[i]);
+      } else if (typeof obj[i] === 'function' || obj[i] === null || obj[i] === undefined) {
+        result += 'null';
+      } else {
+        result += obj[i];
+      }
+      if (i < obj.length - 1) {
+        result += ',';
+      }
+    }
+    result += ']';
+  } else if (typeof obj === 'object') {
+    result += '{';
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      } else {
+        result += stringifyJSON(key) + ':';
+      }
+      if (typeof obj[key] === 'object' || typeof obj[key] === 'string') {
+        result += stringifyJSON(obj[key]) + ',';
+      } else {
+        result += obj[key] + ',';
+      }
+    }
+    if (result.length === 1) {
+      result += '}';
+    } else {
+      result = result.slice(0, -1);
+      result += '}';
+    }
+  }
+  return result;
 };
